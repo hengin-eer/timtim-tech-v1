bb37b4328c2c62e98c018f3486693902dfea6539
hengin-eer <hirokix0608@gmail.com>
2024-03-31 20:30:52 +0900

    💄 config: FontとPrimary Colorの追加

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			"primary-green": "#18EF95",
+			"primary-blue": "#13AED0",
+			"white": "#ffffff",
+			"black": "#000000",
+		},
 		extend: {},
 	},
 	plugins: [],
