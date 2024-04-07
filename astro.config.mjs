b09b93f37fd22dbf981aa69dd4f929897e1c9ae6
hengin-eer <hirokix0608@gmail.com>
2024-04-07 13:03:51 +0900

    🚀config: github pagesへのデプロイ設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://hengin-eer.github.io',
+  base: '/timtim-tech-v1',
 });
