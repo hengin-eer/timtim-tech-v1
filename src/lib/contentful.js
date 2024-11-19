163fa6c0c73b24de1494f11b6aec2452b1ff7949
hengin-eer <hirokix0608@gmail.com>
2024-11-19 23:54:22 +0900

    🚚 #5 tsに変えるだけ

--- src/lib/contentful.js
+++
@@ -1,6 +0,0 @@
-import contentful from 'contentful';
-
-export const contentfulClient = contentful.createClient({
-    space: import.meta.env.CONTENTFUL_SPACE_ID,
-    accessToken: import.meta.env.CONTENTFUL_DELIVERY_TOKEN
-});
