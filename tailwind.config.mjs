08b088852342a8caa90cb5341e15eb7bcc72fd81
hengin-eer <hirokix0608@gmail.com>
2024-11-20 05:29:13 +0900

    🎨 #5 tailwindのカラーパレットの更新

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		colors: {
-			"primary-green": "#18EF95",
-			"primary-blue": "#13AED0",
-			"white": "#ffffff",
-			"black": "#000000",
+			"primary-green": "#18ef95",
+			"primary-dark-green": "#00b86b",
+			"primary-blue": "#13aed0",
+			"primary-red": "#f6444f",
+			white: "#ffffff",
+			"gray-base": "#9ebcca",
+			"gray-light": "#edf0f7",
+			black: "#000000",
 		},
 		extend: {},
 	},
 	plugins: [],
-}
+};
